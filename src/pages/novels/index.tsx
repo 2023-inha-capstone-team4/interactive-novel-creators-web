180658c73af84b567c5b65b32b4ce58aab84b347
Woody <gintooooonic@gmail.com>
2023-05-28 18:49:30 +0900

    테스트 데이터 제거

--- src/pages/novels/index.tsx
+++ src/pages/novels/index.tsx
@@ -6,21 +6,21 @@ import useSWR from 'swr';
  * 내 작품 페이지입니다.
  */
 export default function Novels() {
-  const { data: novels, error } = useSWR('/api-dummy/novels');
-  console.log(novels);
+  // const { data: novels, error } = useSWR('/api-dummy/novels');
+  // console.log(novels);
 
-  if (error) {
-    return <p>에러가 발생했습니다.</p>;
-  }
+  // if (error) {
+  //   return <p>에러가 발생했습니다.</p>;
+  // }
 
-  if (!novels) {
-    return <></>;
-  }
+  // if (!novels) {
+  //   return <></>;
+  // }
 
   return (
     <Layout>
       <h2 style={{ marginTop: 0, marginBottom: 30 }}>내 작품</h2>
-      <NovelList novels={novels} />
+      {/* <NovelList novels={novels} /> */}
     </Layout>
   );
 }
