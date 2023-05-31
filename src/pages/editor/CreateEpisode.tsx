9b8dfd7a3e2a1f97dbe1dcbd90b630ec95bbd60b
Woody <gintooooonic@gmail.com>
2023-05-31 16:38:31 +0900

    refactor: 에디터 주소를 환경 변수로 관리

--- src/pages/editor/CreateEpisode.tsx
+++ src/pages/editor/CreateEpisode.tsx
@@ -33,10 +33,7 @@ export default function CreateEpisode() {
           </ul>
         }
       />
-      <iframe
-        src="http://interactive-novel-editor.s3-website.kr.object.ncloudstorage.com/"
-        ref={iframeRef}
-      />
+      <iframe src={process.env.REACT_APP_EDITOR_URL} ref={iframeRef} />
       <Modal title="에디터 도움말" showing={modalState} onClose={closeModal}>
         <p>에디터의 다양한 기능을 활용하여 인터렉티브 노벨을 쉽고 빠르게 제작해보세요.</p>
       </Modal>
