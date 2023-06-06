a5b7e67e60d885a6fa3772aa70934deaf1db5b40
Woody <gintooooonic@gmail.com>
2023-06-06 16:57:31 +0900

    에디터 프로젝트 추가

--- src/libs/editor/src/index.js
+++ src/libs/editor/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import TestScreen from './component/test_components/TestScreen';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import TestScreen from "./component/test_components/TestScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
@@ -19,3 +19,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 작가 서비스에서 사용
+const EditorApp = App;
+module.exports = {
+  EditorApp,
+};
